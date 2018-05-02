968ee83aafbcd70442fc3eeb4a837798bb706053
james-cain <m15960192140@163.com>
2018-05-02 11:05:32 +0800

    init ssr repo

---
+++ nuxt.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'what-is-the-vue-ssr',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    publicPath: '/assets/',
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    },
+    postcss: [
+      require('postcss-px2rem')(),
+      require('autoprefixer')({
+        browsers: ['last 3 versions']
+      })
+    ]
+  }
+}
