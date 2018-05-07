d1b5ac9d0bc5794b621a3089d5e8fb5a391ec037
james-cain <m15960192140@163.com>
2018-05-07 11:34:48 +0800

    更新服务器地址

--- plugins/axios.js
+++ plugins/axios.js
@@ -5,7 +5,7 @@ let options = {};
 // if (process.server) {
     // options.baseURL = `http://${process.env.HOST || 'localhost'}:${process.env.PORT || 8999}`;
 // }
-options.baseURL = 'http://localhost:9005';
+options.baseURL = 'http://coracain.top';
 options.withCredentials = true;
 console.log(options.baseURL);
 
